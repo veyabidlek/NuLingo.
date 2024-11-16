5d771ef10923bf3b2f0da7f2f28dd308ae7f9cc6
Bektas Keldibayev <bektas.keldibayev@nu.edu.kz>
2024-11-16 14:04:39 +0500

    adds auth

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URL || "mongodb://localhost:27017/lecture1"
-    );
+    await mongoose.connect(process.env.MONGODB_URL || "");
     console.log("MongoDB connected...");
   } catch (err: any) {
     console.error(err.message);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,19 +3,14 @@ import express from "express";
 import connectDB from "./db";
 import globalRouter from "./global-router";
 import { logger } from "./logger";
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 connectDB();
 
 app.use(logger);
 app.use(express.json());
-app.use("/api/v1/", globalRouter);
-
-app.get("/helloworld", (request, response) => {
-  response.send("Hello World!");
-});
+app.use("/api/", globalRouter);
 
 app.listen(PORT, () => {
   console.log(`Server runs at http://localhost:${PORT}`);
